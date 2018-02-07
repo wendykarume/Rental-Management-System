1181bcf8370fe4e90da5fa9aff4b82a463ba20fa
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-02-07 10:33:33 +0300

    updated controller

--- Rental System/src/rental/system/Controller.java
+++ Rental System/src/rental/system/Controller.java
@@ -1,6 +1,8 @@
 package rental.system;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,15 +16,17 @@ import javafx.stage.Stage;
 public class Controller extends Application {
     
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.setTitle("Rental Management System");
-        
-                
-        Scene scene = new Scene(root);
-        
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setTitle("Rental Management System");
+        } catch (IOException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
