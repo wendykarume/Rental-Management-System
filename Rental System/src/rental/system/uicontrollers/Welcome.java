9f51667431733d44a09ad4b664583c1588b3dfcc
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-02-08 00:00:21 +0300

    Welcome Controller Button Functions

---
+++ Rental System/src/rental/system/uicontrollers/Welcome.java
@@ -0,0 +1,22 @@
+package rental.system.uicontrollers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Welcome{
+
+@FXML
+private void signup(ActionEvent event){
+    
+    System.out.println("To Signup");
+    
+}
+
+@FXML
+private void login(ActionEvent event){
+    
+    System.out.println("To Login");
+    
+}
+
+}
