89e6a1dadca66a754159a89839b1beee113dfe71
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-02-07 23:59:29 +0300

    Edit Login Controller

---
+++ Rental System/src/rental/system/uicontrollers/Login.java
@@ -0,0 +1,33 @@
+package rental.system.uicontrollers;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Login extends Application {
+    
+    @Override
+    public void start(Stage primaryStage){
+        
+        try {
+            FXMLLoader loader = new FXMLLoader(); 
+            loader.setLocation(getClass().getResource("/rental/system/ui/login.fxml"));
+            Parent root =loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setTitle("Login | Rental Management System");
+        } catch (IOException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
