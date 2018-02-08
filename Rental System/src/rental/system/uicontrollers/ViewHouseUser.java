3c3f98410e99a62ac4805d640264902c1b39d827
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-02-08 23:00:18 +0300

    Controller for interface

---
+++ Rental System/src/rental/system/uicontrollers/ViewHouseUser.java
@@ -0,0 +1,14 @@
+package rental.system.uicontrollers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ViewHouseUser implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
