6c1788dbc5273a548574324955579867065b1a03
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-09 11:21:27 +0300

    [Update] - Main Class

--- Rental System/src/rental_system/Controller.java
+++ Rental System/src/rental_system/Controller.java
@@ -1,33 +1,7 @@
-package rental.system;
+package rental_system;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Controller{
 
-/**
- *
- * @author maxwell
- */
-public class Controller extends Application {
     
-    @Override
-    public void start(Stage primaryStage){
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("login.fxml"));
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            primaryStage.setTitle("Rental Management System");
-        } catch (IOException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-    
+
 }
