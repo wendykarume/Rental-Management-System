513b4c6a72291687b8af5d1a4715f9677d19dce2
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-09 11:23:07 +0300

    [Edit] - User Interfaces

--- Rental System/src/rental_system/RentalSystem.java
+++ Rental System/src/rental_system/RentalSystem.java
@@ -1,11 +1,28 @@
-package rental.system;
+package rental_system;
 
-/**
- *
- * @author maxwell
- */
-public class RentalSystem{
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class RentalSystem extends Application {
     
-    //code
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Rental Management System");
+        primaryStage.show();
+    }
+    
+    public static void main(String [] args){
+        
+        launch(args);
+        
+    }
     
 }
