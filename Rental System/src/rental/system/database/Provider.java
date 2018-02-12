eeb6063212d623526861a061eca0aeefdb917eb4
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-02-12 17:30:49 +0300

    [Update] - Provider Model

--- Rental System/src/rental/system/database/Provider.java
+++ Rental System/src/rental/system/database/Provider.java
@@ -10,53 +10,68 @@ public class Provider {
    static final String USER = "test";
    static final String PASS = "R3ntal_Syst3m";
    
-   public static void main(String[] args) {
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("com.mysql.jdbc.Driver");
+   public void create(){
+       
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
 
-      //STEP 3: Open a connection
-      System.out.println("Connecting to a given database...");
-      conn = DriverManager.getConnection(DB_URL, USER, PASS);
-      System.out.println("Connected database successfully...");
-      
-      //STEP 4: Execute a query
-      System.out.println("Creating table in given database...");
-      stmt = conn.createStatement();
-      
-      String sql;
-      sql = "CREATE TABLE Providers(ProviderID INTEGER not NULL AUTOINCREMENT, " +
-                   " FirstName TEXT(255), LastName TEXT(255), " + 
-                   " Email VARCHAR(255), Password VARCHAR(255),"+
-                   " PRIMARY KEY ( ProviderID ))";
-      
-      stmt.executeUpdate(sql);
-      System.out.println("Created table in given database...");
-     
-         
-      
-   }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            conn.close();
-      }catch(SQLException se){
-      }// do nothing
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-   System.out.println("Goodbye!");
-}//end main
-}//end JDBCExample
+            String sql;
+            sql = "CREATE TABLE IF NOT EXISTS Provider(ProviderID INTEGER AUTO_INCREMENT, " +
+                        " FirstName TEXT, LastName TEXT, " + 
+                        " Email TEXT, Password TEXT,"+
+                        " PRIMARY KEY(ProviderID))";
+            stmt.executeUpdate(sql);
+            
+        }catch(SQLException se){
+            
+        
+           se.printStackTrace();
+           
+        }catch(Exception e){
+            
+           e.printStackTrace();
+           
+        }finally{
+            
+           try{
+               
+                if(stmt!=null)
+                    
+                    conn.close();
+                
+           }catch(SQLException se){
+           
+           }
+           try{
+               
+                if(conn!=null)
+                    
+                    conn.close();
+           
+           }catch(SQLException se){
+                
+               se.printStackTrace();
+           
+           }
+        
+        }
+
+    }
+   
+   public void insert(){
+       
+       
+       
+   }
+   
+   public void get(){
+       
+       
+       
+   }
+
+}
