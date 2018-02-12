cfd0b3f1b1b74a73a9975a4cba0521a431b1880d
karume wendy <karume wendy@Emocat>
2018-02-12 13:07:07 +0300

    Interface Logic

--- Rental System/src/rental/system/Controller.java
+++ Rental System/src/rental/system/Controller.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,21 +11,15 @@ import javafx.stage.Stage;
 // necessary imports
 public class Controller extends Application {
     // subclassing Application in order to use its methods
-    
-    @Override
-    public void init(){
-                
-    }
+    FXMLLoader load = new FXMLLoader();
     
     @Override
     public void start(Stage stage){
         // overriding in Application to utilise its functionality
         try {
             // Welcome
-            FXMLLoader welcome = new FXMLLoader(); // obj to load fxml
-            welcome.setLocation(getClass().
-                    getResource("/rental/system/ui/welcome.fxml"));
-            Parent root = welcome.load(); // defining root as the welcome
+            load.setLocation(getClass().getResource("/rental/system/ui/welcome.fxml"));
+            Parent root = load.load(); // defining root as the welcome
             Scene scene = new Scene(root);
             stage.setScene(scene);// setting the scene
             stage.show(); // displaying the window
@@ -38,21 +31,8 @@ public class Controller extends Application {
             // catching exception if fxml not found
         }
         
-        finally{
-            
-            stop();
-            
-        }
-        
     }
     
-    @Override
-    public void stop(){
-        
-        
-    }
-    
-    @FXML
     public static void main(String[] args) {
         
         launch(args);
