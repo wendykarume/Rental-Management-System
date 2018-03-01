3520666f72a6ae551c64f433977a0713e883ecd5
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-03-01 17:52:20 +0300

    [Debug] - Database connection

--- Rental System/src/rental/system/models/House.java
+++ Rental System/src/rental/system/models/House.java
@@ -2,18 +2,18 @@ package rental.system.models;
 import java.sql.*;
 
 public class House {
-   // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-   static final String DB_URL = "jdbc:mysql://localhost/HOUSE_RENTAL_SYSTEM";
+    // JDBC driver name and database URL
+     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL = "jdbc:mysql://localhost/HOUSE_RENTAL_SYSTEM";
 
-   //  Database credentials
-   static final String USER = "test";
-   static final String PASS = "R3ntal_Syst3m";
+    //  Database credentials
+    static final String USER = "test";
+    static final String PASS = "R3ntal_Syst3m";
+    Connection conn = null;
+    Statement stmt = null;
    
    public void create(){
-       
-        Connection conn = null;
-        Statement stmt = null;
+
         try{
             Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -24,7 +24,7 @@ public class House {
             sql = "CREATE TABLE IF NOT EXISTS House"
                     + "(HouseID INTEGER AUTO_INCREMENT, "
                     + " HouseLocation TEXT, HouseType TEXT, "
-                    + " HousePrice INTEGER, HouseStatus TEXT "
+                    + " HousePrice TEXT, HouseStatus TEXT, "
                     + " PRIMARY KEY(HouseID))";
             stmt.executeUpdate(sql);
             
@@ -63,22 +63,68 @@ public class House {
 
     }
    
-   public void insert(){
-      String sql = "INSERT INTO Houses (HouseID, HouseType, HouseLocation,"
-            + " HouseStatus, HousePrice)"
-            + " VALUES (?, ?, ?, ?, ?)";
-  
-       
-       
-   }
-   public void get(String type, String location,Integer price,String status){
-       String sql = "SELECT * FROM Users WHERE ((type = ?) and (location = ?) and (status = ?) and (price = ?))"
-               + "(type, location, status, price)";
- 
-  
-       
-       
-       
-   }
+    public void insert(String type, String location, String status, String price)
+            throws SQLException{
 
+        try{
+            // Catching a connection exception
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            
+            // Manually committing data
+            conn.setAutoCommit(false);
+            
+            // Creating a statement to be used while on the connection
+            stmt = conn.createStatement();
+            
+            // Creating statements to be executed
+            String sql = "INSERT INTO House(HouseType,"
+                        + " HouseLocation, HouseStatus,"
+                        +" HousePrice) VALUES(?, ?, ?, ?)";
+            
+            // Creating a stating that will facilitate parsing of data
+            PreparedStatement statement = conn.prepareStatement(sql);
+            
+            // Setting values parsed in function
+            statement.setString(1, type);
+            statement.setString(2, location);
+            statement.setString(3, status);
+            statement.setString(4, price);
+            
+            // Executing statement
+            statement.executeUpdate();
+            
+            // Save changes
+            conn.commit();
+            
+        }catch(SQLException | ClassNotFoundException se){
+            // Revert
+            conn.rollback();
+            
+        }finally{
+            // Close connection if ...
+            try{
+                // there is no statement
+                if(stmt != null)
+                    // Close connection
+                    conn.close();
+                
+            }catch(SQLException se){
+           
+            }
+            try{
+                // there are no database credentials
+                if(conn != null)
+                    // Close connection
+                    conn.close();
+           
+            }catch(SQLException se){
+               
+            }
+        
+        }       
+       
+   }
+       
+       
 }
