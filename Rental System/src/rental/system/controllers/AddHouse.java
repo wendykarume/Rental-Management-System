c5da95ceadbafa077aa16954782947934fc8f447
karume wendy <karume wendy@Emocat>
2018-03-04 10:20:32 +0300

    Adding comments

--- Rental System/src/rental/system/controllers/AddHouse.java
+++ Rental System/src/rental/system/controllers/AddHouse.java
@@ -1,22 +1,34 @@
+// Class package
 package rental.system.controllers;
 
-import com.jfoenix.controls.JFXTextField;
+// Exception handling
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+// JavaFX imports
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import com.jfoenix.controls.JFXTextField;
 import javafx.scene.layout.AnchorPane;
+
+// Class imports
 import rental.system.models.House;
 
+/*
+    Class failitating the provider in adding houses
+*/
 public class AddHouse {
 
+    // FXML objects to be used
     @FXML private AnchorPane add_house;
     @FXML private JFXTextField type, place, status, price;
     
-    House house = new House();
+    // Class objects to be used
+    House house = new House(); // Database class for houses
     
+    // Private method used by FXML button to add new houses
     @FXML private void add(ActionEvent event) throws Exception{
         
         try{
@@ -36,6 +48,7 @@ public class AddHouse {
         }
     }
     
+    // Private method for going back
     @FXML private void back(ActionEvent event){
         
         try{
